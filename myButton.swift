f14c1272ca900f789037c5dd375570328979e3ce
de2414 <de2414@gmail.com>
2017-06-23 00:03:00 +0800

    first commit

---
+++ myButton.swift
@@ -0,0 +1,21 @@
+//
+//  myButton.swift
+//  rollette
+//
+//  Created by 林宥辰 on 2017/1/19.
+//  Copyright © 2017年 林宥辰. All rights reserved.
+//
+
+import UIKit
+
+class myButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
